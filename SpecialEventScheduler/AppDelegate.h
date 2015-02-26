47b06e2cb009979646b3a3f55249b305a2f3a56a
Nathan-Bisson <nathan_bisson@live.com>
2015-02-25 19:14:59 -0500

    Initial Commit

---
+++ SpecialEventScheduler/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SpecialEventScheduler
+//
+//  Created by Nathan Bisson on 2015-02-25.
+//  Copyright (c) 2015 Nathan Bisson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SpecialEventScheduler/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SpecialEventScheduler
+//
+//  Created by Nathan Bisson on 2015-02-25.
+//  Copyright (c) 2015 Nathan Bisson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
